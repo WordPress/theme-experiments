1ec69353b1185af59061c498e704a1422f5cffd8
Andrés <andres.maneiro@automattic.com>
2020-02-14 13:42:12 +0100

    Add resets

--- global-styles/functions.php
+++ global-styles/functions.php
@@ -5,9 +5,17 @@
  */
 function global_styles_experimental_theme_register_styles() {
 	wp_enqueue_style(
+		'global-styles-theme-style-resets',
+		get_stylesheet_directory_uri() . '/resets.css',
+		array(),
+		filemtime( dirname( __FILE__ ) . '/resets.css' )
+	);
+	wp_enqueue_style(
 		'global-styles-theme-style',
 		get_stylesheet_uri(),
-		array(),
+		array(
+			'global-styles-theme-style-resets'
+		),
 		filemtime( dirname( __FILE__ ) . '/style.css' )
 	);
 }
