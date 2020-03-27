ed9c92b687824d96e0e1f58797bcb69a77d494de
Andrés <andres.maneiro@automattic.com>
2020-03-27 19:27:52 +0100

    Adapt to editor styles now that edit-site has support

--- global-styles/functions.php
+++ global-styles/functions.php
@@ -1,27 +1,17 @@
 <?php
 
-/**
- * Register and enqueue styles.
- */
-function global_styles_experimental_theme_register_styles() {
+function global_styles_experimental_after_setup_theme() {
+	add_theme_support('editor-styles');
+	add_editor_style('resets.css');
+}
+add_action( 'after_setup_theme', 'global_styles_experimental_after_setup_theme' );
+
+function global_styles_experimental_wp_enqueue_scripts() {
 	wp_enqueue_style(
 		'global-styles-theme-style-resets',
 		get_stylesheet_directory_uri() . '/resets.css',
 		array(),
 		filemtime( dirname( __FILE__ ) . '/resets.css' )
 	);
-	wp_enqueue_style(
-		'global-styles-theme-style',
-		get_stylesheet_uri(),
-		array(
-			'global-styles-theme-style-resets'
-		),
-		filemtime( dirname( __FILE__ ) . '/style.css' )
-	);
 }
-add_action( 'wp_enqueue_scripts', 'global_styles_experimental_theme_register_styles' );
-
-// Load styles in edit-site,
-// which doesn't use the add_editor_styles registration mechanism
-// so styles aren't wrapped by .editor-styles-wrapper.
-add_action( 'admin_enqueue_scripts', 'global_styles_experimental_theme_register_styles' );
+add_action( 'wp_enqueue_scripts', 'global_styles_experimental_wp_enqueue_scripts' );
