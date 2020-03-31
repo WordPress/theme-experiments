37dcd5b87b8fbfbadfe6c785ab489ae4a3d0eb2d
Andrés <andres.maneiro@automattic.com>
2020-03-31 17:30:18 +0200

    Fix style file name

--- global-styles/functions.php
+++ global-styles/functions.php
@@ -2,7 +2,7 @@
 
 function global_styles_experimental_after_setup_theme() {
 	add_theme_support('editor-styles');
-	add_editor_style('styles.css');
+	add_editor_style('style.css');
 	add_editor_style('editor-styles.css');
 }
 add_action( 'after_setup_theme', 'global_styles_experimental_after_setup_theme' );
@@ -10,9 +10,9 @@ add_action( 'after_setup_theme', 'global_styles_experimental_after_setup_theme' 
 function global_styles_experimental_wp_enqueue_scripts() {
 	wp_enqueue_style(
 		'global-styles-theme-style-resets',
-		get_stylesheet_directory_uri() . '/styles.css',
+		get_stylesheet_directory_uri() . '/style.css',
 		array(),
-		filemtime( dirname( __FILE__ ) . '/styles.css' )
+		filemtime( dirname( __FILE__ ) . '/style.css' )
 	);
 }
 add_action( 'wp_enqueue_scripts', 'global_styles_experimental_wp_enqueue_scripts' );
