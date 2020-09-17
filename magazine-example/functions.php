c0c15b22d1537c6c069ff79662c2624c5a90caf8
Kjell Reigstad <kjell.reigstad@automattic.com>
2020-09-17 12:53:12 -0400

    Use correct editor stylesheet name.
    Co-authored-by: Jeff Ong <jeff.ong@automattic.com>

--- magazine-example/functions.php
+++ magazine-example/functions.php
@@ -31,7 +31,7 @@ if ( ! function_exists( 'magazine_example_support' ) ) :
 		add_theme_support( 'editor-styles' );
 
 		// Enqueue editor styles.
-		add_editor_style( 'editor-editor.css' );
+		add_editor_style( 'style-editor.css' );
 
 		// Support a custom color palette.
 		add_theme_support( 'editor-color-palette', array(
