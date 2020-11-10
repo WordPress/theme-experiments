81d5dc5379b624f5fdc51f6ab83a035eecbdac0a
Kjell Reigstad <kjell@kjellr.com>
2020-11-10 09:44:04 -0500

    Add editor alignments.

--- blockbase/functions.php
+++ blockbase/functions.php
@@ -19,7 +19,10 @@ if ( ! function_exists( 'blockbase_support' ) ) :
 		add_theme_support( 'editor-styles' );
 
 		// Enqueue editor styles.
-		add_editor_style( 'style.css' );
+		add_editor_style( array( 
+			'style.css',
+			'assets/alignments-editor.css'
+		) );
     }
     add_action( 'after_setup_theme', 'blockbase_support' );
 endif;
