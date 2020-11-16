fae49bad8e8e2eb2b0810b28dc2791f1ccb9179c
Kjell Reigstad <kjell@kjellr.com>
2020-11-16 09:19:31 -0500

    Add support for featured images.

--- emptytheme/functions.php
+++ emptytheme/functions.php
@@ -3,7 +3,10 @@
 if ( ! function_exists( 'emptytheme_support' ) ) :
 	function emptytheme_support()  {
 
-		// Alignwide and alignfull classes in the block editor
+		// Adding support for featured images.
+		add_theme_support( 'post-thumbnails' );
+
+		// Adding support for alignwide and alignfull classes in the block editor.
 		add_theme_support( 'align-wide' );
 
 		// Adding support for core block visual styles.
