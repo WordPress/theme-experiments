cf2c4b6222c5cb9b060040ec06d2bf20a278a5fe
Jeff Ong <jonger4@gmail.com>
2020-11-16 11:29:41 -0500

    Monticello boilerplate.

--- monticello/functions.php
+++ monticello/functions.php
@@ -1,7 +1,7 @@
 <?php
 
-if ( ! function_exists( 'emptytheme_support' ) ) :
-	function emptytheme_support()  {
+if ( ! function_exists( 'monticello_support' ) ) :
+	function monticello_support()  {
 
 		// Adding support for featured images.
 		add_theme_support( 'post-thumbnails' );
@@ -24,18 +24,18 @@ if ( ! function_exists( 'emptytheme_support' ) ) :
 		// Enqueue editor styles.
 		add_editor_style( 'style.css' );
     }
-    add_action( 'after_setup_theme', 'emptytheme_support' );
+    add_action( 'after_setup_theme', 'monticello_support' );
 endif;
 
 /**
  * Enqueue scripts and styles.
  */
-function emptytheme_scripts() {
+function monticello_scripts() {
 
 	// Enqueue theme stylesheet.
-	wp_enqueue_style( 'emptytheme-style', get_template_directory_uri() . '/style.css', array(), wp_get_theme()->get( 'Version' ) );
+	wp_enqueue_style( 'monticello-style', get_template_directory_uri() . '/style.css', array(), wp_get_theme()->get( 'Version' ) );
 
 	// Enqueue alignments stylesheet.
-	wp_enqueue_style( 'emptytheme-alignments-style', get_template_directory_uri() . '/assets/alignments-front.css', array(), wp_get_theme()->get( 'Version' ) );
+	wp_enqueue_style( 'monticello-alignments-style', get_template_directory_uri() . '/assets/alignments-front.css', array(), wp_get_theme()->get( 'Version' ) );
 }
-add_action( 'wp_enqueue_scripts', 'emptytheme_scripts' );
+add_action( 'wp_enqueue_scripts', 'monticello_scripts' );
