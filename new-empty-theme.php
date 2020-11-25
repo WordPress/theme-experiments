722af89a162cfa93bf93a1b954795dd1ffc92fe4
Maggie Cabrera <maggie.cabrera@automattic.com>
2020-11-25 19:08:48 +0100

    better treatment of space in the theme name

--- new-empty-theme.php
+++ new-empty-theme.php
@@ -13,7 +13,8 @@ class Generate_Theme {
 		$handle = fopen ("php://stdin","r");
 
 		$this->theme['name'] = trim(fgets($handle));
-		$this->theme['slug'] = urlencode($this->theme['name']);
+		$this->theme['slug'] = $this->sanitize_title_with_dashes($this->theme['name']);
+		$this->theme['functions_slug'] = str_replace( '-', '_', $this->theme['slug'] );
 		$this->create_zip();
 	}
 
@@ -79,9 +80,88 @@ class Generate_Theme {
 
 		// Special treatment for functions.php
 		if ( 'functions.php' === $filename ) {
-			$contents = str_replace( 'emptytheme', $this->theme['slug'], $contents );
+			$contents = str_replace( 'emptytheme', $this->theme['functions_slug'], $contents );
 			return $contents;
 		}
+	}
+
+	function sanitize_title_with_dashes( $title, $raw_title = '', $context = 'display' ) {
+		$title = strip_tags( $title );
+		// Preserve escaped octets.
+		$title = preg_replace( '|%([a-fA-F0-9][a-fA-F0-9])|', '---$1---', $title );
+		// Remove percent signs that are not part of an octet.
+		$title = str_replace( '%', '', $title );
+		// Restore octets.
+		$title = preg_replace( '|---([a-fA-F0-9][a-fA-F0-9])---|', '%$1', $title );
+	 
+		$title = strtolower( $title );
+	 
+		if ( 'save' === $context ) {
+			// Convert &nbsp, &ndash, and &mdash to hyphens.
+			$title = str_replace( array( '%c2%a0', '%e2%80%93', '%e2%80%94' ), '-', $title );
+			// Convert &nbsp, &ndash, and &mdash HTML entities to hyphens.
+			$title = str_replace( array( '&nbsp;', '&#160;', '&ndash;', '&#8211;', '&mdash;', '&#8212;' ), '-', $title );
+			// Convert forward slash to hyphen.
+			$title = str_replace( '/', '-', $title );
+	 
+			// Strip these characters entirely.
+			$title = str_replace(
+				array(
+					// Soft hyphens.
+					'%c2%ad',
+					// &iexcl and &iquest.
+					'%c2%a1',
+					'%c2%bf',
+					// Angle quotes.
+					'%c2%ab',
+					'%c2%bb',
+					'%e2%80%b9',
+					'%e2%80%ba',
+					// Curly quotes.
+					'%e2%80%98',
+					'%e2%80%99',
+					'%e2%80%9c',
+					'%e2%80%9d',
+					'%e2%80%9a',
+					'%e2%80%9b',
+					'%e2%80%9e',
+					'%e2%80%9f',
+					// Bullet.
+					'%e2%80%a2',
+					// &copy, &reg, &deg, &hellip, and &trade.
+					'%c2%a9',
+					'%c2%ae',
+					'%c2%b0',
+					'%e2%80%a6',
+					'%e2%84%a2',
+					// Acute accents.
+					'%c2%b4',
+					'%cb%8a',
+					'%cc%81',
+					'%cd%81',
+					// Grave accent, macron, caron.
+					'%cc%80',
+					'%cc%84',
+					'%cc%8c',
+				),
+				'',
+				$title
+			);
+	 
+			// Convert &times to 'x'.
+			$title = str_replace( '%c3%97', 'x', $title );
+		}
+	 
+		// Kill entities.
+		$title = preg_replace( '/&.+?;/', '', $title );
+		$title = str_replace( '.', '-', $title );
+	 
+		$title = preg_replace( '/[^%a-z0-9 _-]/', '', $title );
+		$title = preg_replace( '/\s+/', '-', $title );
+		$title = preg_replace( '|-+|', '-', $title );
+		$title = trim( $title, '-' );
+	 
+		return $title;
 	}
 }
 
