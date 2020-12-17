4f408f356fdd94c24d5445fdb8b4b94366408e7a
Kjell Reigstad <kjell@kjellr.com>
2020-12-17 08:04:09 -0500

    Update subpackage.

--- tt1-blocks/inc/block-styles.php
+++ tt1-blocks/inc/block-styles.php
@@ -5,7 +5,7 @@
  * @link https://developer.wordpress.org/reference/functions/register_block_style/
  *
  * @package WordPress
- * @subpackage Twenty_Twenty_One
+ * @subpackage TT1_Blocks
  * @since 1.0.0
  */
 
