f4a013188ed45bd56d31f7a1bd291c3da7ff5502
Kjell Reigstad <kjell@kjellr.com>
2020-12-18 09:07:01 -0500

    Move default query into its own template part.
    - Tidy up the query itself.
    - Fix wide/full styles for inner content in the query block.

---
+++ tt1-blocks/footer.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Unused file.
+ *
+ * This file is only present to pass the Theme Check. It's not used by the theme.
+ */
+?>
+
+<?php wp_footer(); ?>
+</body>
---
+++ tt1-blocks/header.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Unused file.
+ *
+ * This file is only present to pass the Theme Check. It's not used by the theme.
+ */
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<?php do_action( 'wp_body_open' ); ?>
