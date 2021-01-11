00c9148f884c83ddce33a4bed8157b7b96a15d37
Kjell Reigstad <kjell.reigstad@automattic.com>
2021-01-11 09:26:56 -0500

    Add a code comment referencing WordPress/gutenberg#28045.

--- tt1-blocks/comments.php
+++ tt1-blocks/comments.php
@@ -3,7 +3,8 @@
  * The template for displaying comments
  *
  * This is the template that displays the area of the page that contains both the current comments
- * and the comment form.
+ * and the comment form. This can be deleted from TT1 Blocks once this Gutenberg issue has been resolved:
+ * https://github.com/WordPress/gutenberg/issues/28045
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
