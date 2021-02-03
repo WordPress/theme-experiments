59970d2496b5371649e15f338331aea4b60d1777
Jason Crist <jcrist@pbking.com>
2021-02-03 13:54:33 -0500

    Create a theme experiment to demo dynamic blocks..
    ...used in classic themes.

---
+++ goose/index.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>
+
+<p> If you are on a page that should be showing a post (such as <a href="/?page_id=2">this</a>) then below you should see the title:</p>
+
+<?php the_title() ?>
+
+<p>There?  Did you see it?</p>
+
+<p>If this were a "FSE" theme then instead of using <code>the_title()</code> tag as the above is doing we could also do the following to show the title:</p>
+
+<pre>
+&lt;!-- wp:post-title /--&gt;
+</pre>
+
+<p>But you can tell by the emptyness below that it does not work:</p>
+
+<!-- wp:post-title /-->
+
+<p>There? Did you see it?  I didn't either...</p>
+
+<?php get_footer(); ?>
---
+++ goose/readme.md
@@ -0,0 +1,3 @@
+This is a (very) bare-bones experimental theme that attempts to use the new dynamic blocks that are available to FSE themes in classic theme PHP files.
+
+It's usefullness ends there.
