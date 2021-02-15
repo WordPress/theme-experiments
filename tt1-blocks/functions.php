683161af150088bbdac9af7ba015e8488e34a7fb
Jason Crist <jcrist@pbking.com>
2021-02-15 15:51:04 -0500

    Experiment: Colorize TT1-blocks with customizer
    Using theme.json to define contents of customizer and leverage the
    customized results in theme.json.

--- tt1-blocks/functions.php
+++ tt1-blocks/functions.php
@@ -57,169 +57,6 @@ if ( ! function_exists( 'tt1_blocks_setup' ) ) {
 			'./assets/css/style-editor.css', 
 		) );
 
-		// Add custom editor font sizes.
-		add_theme_support(
-			'editor-font-sizes',
-			array(
-				array(
-					'name'      => esc_html__( 'Extra small', 'tt1-blocks' ),
-					'shortName' => esc_html_x( 'XS', 'Font size', 'tt1-blocks' ),
-					'size'      => 16,
-					'slug'      => 'extra-small',
-				),
-				array(
-					'name'      => esc_html__( 'Small', 'tt1-blocks' ),
-					'shortName' => esc_html_x( 'S', 'Font size', 'tt1-blocks' ),
-					'size'      => 18,
-					'slug'      => 'small',
-				),
-				array(
-					'name'      => esc_html__( 'Normal', 'tt1-blocks' ),
-					'shortName' => esc_html_x( 'M', 'Font size', 'tt1-blocks' ),
-					'size'      => 20,
-					'slug'      => 'normal',
-				),
-				array(
-					'name'      => esc_html__( 'Large', 'tt1-blocks' ),
-					'shortName' => esc_html_x( 'L', 'Font size', 'tt1-blocks' ),
-					'size'      => 24,
-					'slug'      => 'large',
-				),
-				array(
-					'name'      => esc_html__( 'Extra Large', 'tt1-blocks' ),
-					'shortName' => esc_html_x( 'XL', 'Font size', 'tt1-blocks' ),
-					'size'      => 40,
-					'slug'      => 'extra-large',
-				),
-				array(
-					'name'      => esc_html__( 'Huge', 'tt1-blocks' ),
-					'shortName' => esc_html_x( 'XXL', 'Font size', 'tt1-blocks' ),
-					'size'      => 96,
-					'slug'      => 'huge',
-				),
-				array(
-					'name'      => esc_html__( 'Gigantic', 'tt1-blocks' ),
-					'shortName' => esc_html_x( 'XXXL', 'Font size', 'tt1-blocks' ),
-					'size'      => 144,
-					'slug'      => 'gigantic',
-				),
-			)
-		);
-
-		// Editor color palette.
-		$black     = '#000000';
-		$dark_gray = '#28303D';
-		$gray      = '#39414D';
-		$green     = '#D1E4DD';
-		$blue      = '#D1DFE4';
-		$purple    = '#D1D1E4';
-		$red       = '#E4D1D1';
-		$orange    = '#E4DAD1';
-		$yellow    = '#EEEADD';
-		$white     = '#FFFFFF';
-
-		add_theme_support(
-			'editor-color-palette',
-			array(
-				array(
-					'name'  => esc_html__( 'Black', 'tt1-blocks' ),
-					'slug'  => 'black',
-					'color' => $black,
-				),
-				array(
-					'name'  => esc_html__( 'Dark Gray', 'tt1-blocks' ),
-					'slug'  => 'dark-gray',
-					'color' => $dark_gray,
-				),
-				array(
-					'name'  => esc_html__( 'Gray', 'tt1-blocks' ),
-					'slug'  => 'gray',
-					'color' => $gray,
-				),
-				array(
-					'name'  => esc_html__( 'Green', 'tt1-blocks' ),
-					'slug'  => 'green',
-					'color' => $green,
-				),
-				array(
-					'name'  => esc_html__( 'Blue', 'tt1-blocks' ),
-					'slug'  => 'blue',
-					'color' => $blue,
-				),
-				array(
-					'name'  => esc_html__( 'Purple', 'tt1-blocks' ),
-					'slug'  => 'purple',
-					'color' => $purple,
-				),
-				array(
-					'name'  => esc_html__( 'Red', 'tt1-blocks' ),
-					'slug'  => 'red',
-					'color' => $red,
-				),
-				array(
-					'name'  => esc_html__( 'Orange', 'tt1-blocks' ),
-					'slug'  => 'orange',
-					'color' => $orange,
-				),
-				array(
-					'name'  => esc_html__( 'Yellow', 'tt1-blocks' ),
-					'slug'  => 'yellow',
-					'color' => $yellow,
-				),
-				array(
-					'name'  => esc_html__( 'White', 'tt1-blocks' ),
-					'slug'  => 'white',
-					'color' => $white,
-				),
-			)
-		);
-
-		add_theme_support(
-			'editor-gradient-presets',
-			array(
-				array(
-					'name'     => esc_html__( 'Purple to Yellow', 'tt1-blocks' ),
-					'gradient' => 'linear-gradient(160deg, ' . $purple . ', ' . $yellow . ')',
-					'slug'     => 'purple-to-yellow',
-				),
-				array(
-					'name'     => esc_html__( 'Yellow to Purple', 'tt1-blocks' ),
-					'gradient' => 'linear-gradient(160deg, ' . $yellow . ', ' . $purple . ')',
-					'slug'     => 'yellow-to-purple',
-				),
-				array(
-					'name'     => esc_html__( 'Green to Yellow', 'tt1-blocks' ),
-					'gradient' => 'linear-gradient(160deg, ' . $green . ', ' . $yellow . ')',
-					'slug'     => 'green-to-yellow',
-				),
-				array(
-					'name'     => esc_html__( 'Yellow to Green', 'tt1-blocks' ),
-					'gradient' => 'linear-gradient(160deg, ' . $yellow . ', ' . $green . ')',
-					'slug'     => 'yellow-to-green',
-				),
-				array(
-					'name'     => esc_html__( 'Red to Yellow', 'tt1-blocks' ),
-					'gradient' => 'linear-gradient(160deg, ' . $red . ', ' . $yellow . ')',
-					'slug'     => 'red-to-yellow',
-				),
-				array(
-					'name'     => esc_html__( 'Yellow to Red', 'tt1-blocks' ),
-					'gradient' => 'linear-gradient(160deg, ' . $yellow . ', ' . $red . ')',
-					'slug'     => 'yellow-to-red',
-				),
-				array(
-					'name'     => esc_html__( 'Purple to Red', 'tt1-blocks' ),
-					'gradient' => 'linear-gradient(160deg, ' . $purple . ', ' . $red . ')',
-					'slug'     => 'purple-to-red',
-				),
-				array(
-					'name'     => esc_html__( 'Red to Purple', 'tt1-blocks' ),
-					'gradient' => 'linear-gradient(160deg, ' . $red . ', ' . $purple . ')',
-					'slug'     => 'red-to-purple',
-				),
-			)
-		);
-
 		// Add support for responsive embedded content.
 		add_theme_support( 'responsive-embeds' );
 
@@ -241,6 +78,10 @@ add_action( 'after_setup_theme', 'tt1_blocks_setup' );
  */
 function tt1_blocks_scripts() {
 	wp_enqueue_style( 'tt1-blocks-style', get_template_directory_uri() . '/style.css', array(), wp_get_theme()->get( 'Version' ) );
+
+	// Enqueue customizer bridge styles 
+	wp_enqueue_style( 'customizer-style', get_template_directory_uri() . '/assets/css/noop.css', array(), wp_get_theme()->get( 'Version' ) );
+
 }
 add_action( 'wp_enqueue_scripts', 'tt1_blocks_scripts' );
 
@@ -262,3 +103,6 @@ require get_template_directory() . '/inc/block-patterns.php';
 
 // Block Styles.
 require get_template_directory() . '/inc/block-styles.php';
+
+// Customizer Bridge
+require get_template_directory() . '/assets/classes/customizer-bridge.php';
