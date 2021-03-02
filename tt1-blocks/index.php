e8727a6894bbbc9a5dbde72c21e8fb8e9f7a1c54
Birgit Pauli-Haack <birgit.pauli@gmail.com>
2021-03-02 12:38:09 -0500

    Update index.php
    It explains, why there is nothing to see.

--- tt1-blocks/index.php
+++ tt1-blocks/index.php
@@ -1,1 +1,3 @@
-// Nothing to see here.
+// Nothing to see here. 
+// This theme was built for the WordPress full-site-editing feature. 
+// You need to install and activate the Gutenberg plugin to make it work. 
