d0cc4c7dfa07a07084d2d82b5d6d15d4449fd263
Birgit Pauli-Haack <birgit.pauli@gmail.com>
2021-03-03 18:06:52 -0500

    Update index.php
    More tweaks of the copy

--- tt1-blocks/index.php
+++ tt1-blocks/index.php
@@ -5,6 +5,6 @@
  */
 
 if ( current_user_can( 'activate_plugins' ) ) {
-	esc_html_e( 'This theme was built for the WordPress full-site-editing feature. You need to install and activate the Gutenberg plugin to make it work. ', 'tt1-blocks' );
+	esc_html_e( 'This theme was built for the WordPress experimental full-site-editing feature. You need to install and activate the Gutenberg plugin to make it work. ', 'tt1-blocks' );
 }
 
