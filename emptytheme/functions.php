fe175fef60cd3ed0a96d2ef7e2486fad1adf34b9
Ben Dwyer <ben@scruffian.com>
2021-03-18 13:03:22 +0000

    Update empty theme to use Gutenberg alignments

--- emptytheme/functions.php
+++ emptytheme/functions.php
@@ -31,11 +31,8 @@ endif;
  * Enqueue scripts and styles.
  */
 function emptytheme_scripts() {
-
 	// Enqueue theme stylesheet.
 	wp_enqueue_style( 'emptytheme-style', get_template_directory_uri() . '/style.css', array(), wp_get_theme()->get( 'Version' ) );
-
-	// Enqueue alignments stylesheet.
-	wp_enqueue_style( 'emptytheme-alignments-style', get_template_directory_uri() . '/assets/alignments-front.css', array(), wp_get_theme()->get( 'Version' ) );
 }
+
 add_action( 'wp_enqueue_scripts', 'emptytheme_scripts' );
