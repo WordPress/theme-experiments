bf7ab4db0685425983fe0adb63d8aa7606d9d37b
Kjell Reigstad <kjell@kjellr.com>
2021-12-15 14:09:23 -0500

    Rename folders, remove unnecessary enqueues.

--- emptytheme/functions.php
+++ emptytheme/functions.php
@@ -3,23 +3,11 @@
 if ( ! function_exists( 'emptytheme_support' ) ) :
 	function emptytheme_support()  {
 
-		// Adding support for featured images.
-		add_theme_support( 'post-thumbnails' );
-
 		// Adding support for core block visual styles.
 		add_theme_support( 'wp-block-styles' );
 
-		// Adding support for responsive embedded content.
-		add_theme_support( 'responsive-embeds' );
-
-		// Add support for editor styles.
-		add_theme_support( 'editor-styles' );
-
 		// Enqueue editor styles.
 		add_editor_style( 'style.css' );
-
-		// Add support for custom units.
-		add_theme_support( 'custom-units' );
 	}
 	add_action( 'after_setup_theme', 'emptytheme_support' );
 endif;
